0bcd651b2a5eb23dfbc73b45a7fca100c35e437e
bodakhesamiksha-dev <bodakhesamiksha2004@gmail.com>
2026-08-30 16:52:05 +0530

    Allow dashboard CORS

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "https://zerodha-frontend-1nbf.onrender.com",
+      "https://zerodha-dashboard-ox82.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
